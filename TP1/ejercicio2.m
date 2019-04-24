6b41eec740ec01bd2f05c03f72247af3e59c1cb6
lucas <nesgnimmehl@gmail.com>
2019-04-24 14:37:22 -0300

    2d y correciones

--- TP1/ejercicio2.m
+++ TP1/ejercicio2.m
@@ -7,7 +7,7 @@ function iteraciones = cifras_significativas_trapecios(file_id, d)
     resultado_trapecios = erf_trapecios(1, n);
     error_trapecios = erf1 - resultado_trapecios;
     n++;
-  until (abs(error_trapecios) < 10^(-d));
+  until (abs(error_trapecios) < 0.5*10^(-d));
   iteraciones = --n;
   fprintf(file_id, ...
       "Iteraciones de erf_trapecios para %d cifras significativas: %d\n resultado=%.10f error=%.10e\n\n"...
@@ -21,14 +21,14 @@ fprintf(file_id, "erf(1) = %.10f (octave) \n\n", erf1);
 fprintf(file_id, "%3s %17s %17s %17s %17s\n","n","res_taylor","error_taylor","res_trapecios","error_trapecios");
 for n = 1:10
   resultado_taylor = erf_taylor(1,n);
-  error_taylor = erf1 - resultado_taylor;
+  error_taylor = abs(erf1 - resultado_taylor);
   resultado_trapecios = erf_trapecios(1,n);
-  error_trapecios = erf1 - resultado_trapecios;
+  error_trapecios = abs(erf1 - resultado_trapecios);
   fprintf(file_id, "%3d %17.10f %17.10e %17.10f %17.10e\n", n, resultado_taylor, error_taylor, resultado_trapecios, error_trapecios);
 end
 
 fprintf(file_id, "\n\n");
-for cifras_significativas = 5:8
+for cifras_significativas = 5:7
   cifras_significativas_trapecios(file_id, cifras_significativas);
 end
 fclose(file_id);
